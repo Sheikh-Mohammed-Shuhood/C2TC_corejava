f35d9c081a9cffc75cbcc4edad229b39e762bb56
Sheikh-Mohammed-Shuhood <shuhood2003@gmail.com>
2023-09-15 18:41:35 +0530

    Third commit

---
+++ Day1/src/Day1/Strings.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String var = new String("Hello world");
+		System.out.println(var);
+
+	}
+
+}
