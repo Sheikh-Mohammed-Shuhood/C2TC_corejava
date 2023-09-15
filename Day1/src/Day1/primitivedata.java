64373a7610e1f8bcd99b8bdd7b32a59120fd5c17
Sheikh-Mohammed-Shuhood <shuhood2003@gmail.com>
2023-09-15 18:36:37 +0530

    First commit

---
+++ Day1/src/Day1/primitivedata.java
@@ -0,0 +1,32 @@
+package Day1;
+
+public class primitivedata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int max = 2147483647;
+		int min = -2147483648;
+		short shortMax = 32767;
+		short shortMin = -32768;
+		long longMax = 9223372036854775807L;
+		long longMin = -9223372036854775808L;
+		byte byteMax = 127;
+		byte byteMin = -128;
+		
+
+		int value1 = 9/2;
+		float value2 = 10f/6f;
+		double value3 = 10d/6d;
+		System.out.println("value  1 = "+ value1);
+		System.out.println("value  2 = "+ value2);
+		System.out.println("value  3 = "+ value3);
+	    int marker = 512;
+	    double percentage = marker * 0.46f;
+	    
+	    System.out.println("percentage:"+percentage);
+
+
+
+	}
+
+}
